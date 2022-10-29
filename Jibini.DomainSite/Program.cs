1c02d999755d359b318fa1282d32b9d49f7611a1
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2022-10-29 10:47:38 -0500

    Add project files.

---
+++ Jibini.DomainSite/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapControllers();
+app.MapBlazorHub();
+app.MapRazorPages();
+app.MapFallbackToPage("/_Error404");
+
+app.Run();
