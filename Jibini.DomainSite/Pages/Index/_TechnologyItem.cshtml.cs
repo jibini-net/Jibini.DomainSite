f2b8f845dfce20bc178b95cabde346ece46f03d0
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2022-10-29 22:17:09 -0500

    Icons and sticky scroll header

--- Jibini.DomainSite/Pages/Index/_TechnologyItem.cshtml.cs
+++ Jibini.DomainSite/Pages/Index/_TechnologyItem.cshtml.cs
@@ -14,7 +14,7 @@ public class TechnologyItem
         {
             Name = "ASP.NET Core (MVC/Blazor)",
             IconClass = "fa fa-globe",
-            Description = "Interactive UI with server and client side code; product template and framework design"
+            Description = "Interactive UI with server- and client-side code; product template and framework design"
         },
         new()
         {
@@ -25,14 +25,44 @@ public class TechnologyItem
         new()
         {
             Name = "Spring Boot",
-            IconClass = "fa fa-globe",
-            Description = "Java and Kotlin backends with Hibernate as an O/RM, used in combination with React.js"
+            IconClass = "fa fa-leaf",
+            Description = "Java and Kotlin backends using an O/RM, used in combination with React.js"
+        },
+        new()
+        {
+            Name = "React.js and Node.js",
+            IconClass = "fab fa-js",
+            Description = "JS and TypeScript frontends for pure JS stacks, or other APIs in .NET Core/Java"
         },
         new()
         {
             Name = "Windows Server",
             IconClass = "fab fa-microsoft",
             Description = "Deployment to on-premises servers and IaaS (Azure, AWS, HostWinds, and more)"
+        },
+        new()
+        {
+            Name = "Linux and UNIX-likes",
+            IconClass = "fab fa-freebsd",
+            Description = "System and cluster administration with heavily virtualized workloads, ZFS filesystem"
+        },
+        new()
+        {
+            Name = "Git and SCM",
+            IconClass = "fab fa-git",
+            Description = "Promoting general code cleanliness and trackability with point-in-time restore"
+        },
+        new()
+        {
+            Name = "OpenGL Graphics",
+            IconClass = "fa fa-gamepad",
+            Description = "Desktop and embedded graphics for games and detailed rendering with shader pipelines"
+        },
+        new()
+        {
+            Name = "C and Low-level Code",
+            IconClass = "fa fa-microchip",
+            Description = "OS kernel and embedded development with a focus on memory safety and portability"
         },
     };
 };
